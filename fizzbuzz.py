fa2aa9d04009497aca97386c53d1d542d5005a40
Seamus Riordan <seamus@riordan.io>
2019-04-12 10:46:02 -0500

    Add in multiple checks

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,11 +8,22 @@ class FizzBuzz(object):
     '''
     classdocs
     '''
+    
+    defaultLength = 15
 
-
-    def __init__(self, length):
+    def __init__(self, length = defaultLength):
         '''
         Constructor
         '''
         self.length = length
         
+        
+    def isMultipleThree(integerToCheck):
+        if integerToCheck % 3 == 0:
+            return True
+        return False
+
+    def isMultipleFive(integerToCheck):
+        if integerToCheck % 5 == 0:
+            return True
+        return False        
--- fizzbuzztest.py
+++ fizzbuzztest.py
@@ -9,12 +9,25 @@ from fizzbuzz.fizzbuzz import *
 
 class TestFizzBuzz(unittest.TestCase):
 
+    def testNoConstructor(self):
+        fizzbuzzInstance = FizzBuzz()
+        self.assertEqual(fizzbuzzInstance.defaultLength, fizzbuzzInstance.length, "Default length not set")
 
-    def testFizzBuzzLength(self):
+
+    def testLength(self):
         trialLength = 15
         fizzbuzzInstance = FizzBuzz(trialLength)
         self.assertEqual(fizzbuzzInstance.length, trialLength, "Length not set")
+        
+    def testIsMultipleThree(self):
+        fizzbuzzInstance = FizzBuzz()
+        self.assertTrue(FizzBuzz.isMultipleThree(3))
+        self.assertFalse(FizzBuzz.isMultipleThree(5))
 
+    def testIsMultipleFive(self):
+        fizzbuzzInstance = FizzBuzz()
+        self.assertFalse(FizzBuzz.isMultipleFive(3))
+        self.assertTrue(FizzBuzz.isMultipleFive(5))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
