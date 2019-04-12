fca828abfe58575185f43990de096829e4d502a8
Seamus Riordan <seamus@riordan.io>
2019-04-12 11:18:34 -0500

    Working fizzbuzz string

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -17,13 +17,32 @@ class FizzBuzz(object):
         '''
         self.length = length
         
-        
-    def isMultipleThree(integerToCheck):
-        if integerToCheck % 3 == 0:
+    def generate(self):
+        evalutedString = ""
+        for i in range(1,self.length+1):
+            evalutedString += FizzBuzz.evaluateStep(i)
+            if not self.isLastStep(i):
+                evalutedString += " "
+        return evalutedString
+    
+    def isLastStep(self, step):
+        if step == self.length:
             return True
         return False
-
-    def isMultipleFive(integerToCheck):
-        if integerToCheck % 5 == 0:
+        
+    def evaluateStep(step):
+        isMultThree = FizzBuzz.isMultiple(3, step)
+        isMultFive  = FizzBuzz.isMultiple(5, step)
+        
+        if isMultThree and isMultFive:
+            return "Fizz Buzz"
+        if isMultThree:
+            return "Fizz"
+        if isMultFive:
+            return "Buzz"
+        return str(step)
+    
+    def isMultiple(multiple, integerToCheck):
+        if integerToCheck % multiple == 0:
             return True
-        return False        
+        return False     
--- fizzbuzztest.py
+++ fizzbuzztest.py
@@ -5,6 +5,7 @@ Created on Apr 12, 2019
 '''
 import unittest
 from fizzbuzz.fizzbuzz import *
+import fizzbuzz
 
 
 class TestFizzBuzz(unittest.TestCase):
@@ -19,15 +20,38 @@ class TestFizzBuzz(unittest.TestCase):
         fizzbuzzInstance = FizzBuzz(trialLength)
         self.assertEqual(fizzbuzzInstance.length, trialLength, "Length not set")
         
-    def testIsMultipleThree(self):
+    def testIsMultiple(self):
         fizzbuzzInstance = FizzBuzz()
-        self.assertTrue(FizzBuzz.isMultipleThree(3))
-        self.assertFalse(FizzBuzz.isMultipleThree(5))
+        testCases = [(True, 3, 6), (False, 3, 5), (True, 5, 15), (False, 5, 7)]
 
-    def testIsMultipleFive(self):
-        fizzbuzzInstance = FizzBuzz()
-        self.assertFalse(FizzBuzz.isMultipleFive(3))
-        self.assertTrue(FizzBuzz.isMultipleFive(5))
+        for testType, multiple, integerToCheck in testCases:
+            self.multipleofIntTest(testType, multiple, integerToCheck)
+        
+    def multipleofIntTest(self, testType, multiple, toCheck):
+        if testType:
+            testFunction = self.assertTrue
+            messageForm = "Returned %d is not mutiple of %d"
+        else:
+            testFunction = self.assertFalse
+            messageForm = "Returned %d is mutiple of %d"
+
+        testFunction(FizzBuzz.isMultiple(multiple, toCheck), messageForm  % (multiple, toCheck))
+
+    def testStep(self):
+        testCases = [(3, "Fizz"), (5, "Buzz"), (7, "7"), (15, "Fizz Buzz")]
+        for stepNumber, expectedResult in testCases:
+            result = FizzBuzz.evaluateStep(stepNumber)
+            self.assertEqual(result, expectedResult, "Got %s expected %s" % (result, expectedResult))
+            
+    def testLastStep(self):
+        fizzbuzzInstance = FizzBuzz(7)
+        self.assertFalse(fizzbuzzInstance.isLastStep(5), "5 is not last step")
+        self.assertTrue(fizzbuzzInstance.isLastStep(7), "7 should be last step")
+            
+    def testRun(self):
+        fizzbuzzInstance = FizzBuzz(16)
+        expectedString = "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 Fizz Buzz 16"
+        self.assertEqual(expectedString, fizzbuzzInstance.generate(), "Run case failed")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
